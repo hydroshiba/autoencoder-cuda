b2a4ff35f988856fab067cf4d7871ba47459c798
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2025-12-15 16:02:01 +0700

    Fix: add log for dataset

--- header/tensor.hpp
+++ header/tensor.hpp
@@ -5,44 +5,44 @@
 
 class Tensor
 {
-    private:
-        std::vector<float> host_data;
-        float *device_data;
-        int N, C, H, W;
-        bool on_gpu;
+private:
+    std::vector<float> host_data;
+    float *device_data;
+    int N, C, H, W;
+    bool on_gpu;
 
-    public:
+public:
     // Constructors and destructor
-        Tensor();
-        Tensor(int n, int c, int h, int w);
-        Tensor(const Tensor &other);
-        Tensor &operator=(const Tensor &other);
-        ~Tensor();
-        
+    Tensor();
+    Tensor(int n, int c, int h, int w);
+    Tensor(const Tensor &other);
+    Tensor &operator=(const Tensor &other);
+    ~Tensor();
+
     // Information and manipulation
-        void resize(int n, int c, int h, int w);
-        int batch() const;
-        int channels() const;
-        
-        int height() const;
-        int width() const;
-        
-        size_t size() const;
+    void resize(int n, int c, int h, int w);
+    int batch() const;
+    int channels() const;
+
+    int height() const;
+    int width() const;
+
+    size_t size() const;
 
     // Get element functions
-        std::size_t index(int n, int c, int h, int w) const;
+    std::size_t index(int n, int c, int h, int w) const;
 
-        // Access to underlying data -> element-wise
-        float &operator()(int n, int c, int h, int w);
-        const float &operator()(int n, int c, int h, int w) const;
+    // Access to underlying data -> element-wise
+    float &operator()(int n, int c, int h, int w);
+    const float &operator()(int n, int c, int h, int w) const;
 
     // Other functions
-        Tensor& to_gpu();
-        Tensor& to_cpu();
-        bool is_gpu() const;
-        
-        float* data();
-        const float* data() const;
+    Tensor &to_gpu();
+    Tensor &to_cpu();
+    bool is_gpu() const;
 
-        friend class Layer;
+    float *data();
+    const float *data() const;
+
+    friend class Layer;
 };
--- source/tensor.cpp
+++ source/tensor.cpp
@@ -3,21 +3,28 @@
 #include <algorithm>
 #include <stdexcept>
 
-Tensor::Tensor() : 
-	host_data(), device_data(nullptr),
-	N(0), C(0), H(0), W(0),
-	on_gpu(false) {}
+Tensor::Tensor() : host_data(), device_data(nullptr),
+				   N(0), C(0), H(0), W(0),
+				   on_gpu(false) {}
 
-Tensor::Tensor(int n, int c, int h, int w) : Tensor() {
+Tensor::Tensor(const Tensor &other) :
+    host_data(other.host_data),
+    device_data(nullptr),
+    N(other.N), C(other.C), H(other.H), W(other.W),
+    on_gpu(false)
+{}
+
+Tensor::Tensor(int n, int c, int h, int w) : Tensor()
+{
 	resize(n, c, h, w);
 }
 
-Tensor::Tensor(const Tensor &other) :
-	host_data(other.host_data), device_data(nullptr),
-	 N(other.N), C(other.C), H(other.H), W(other.W), on_gpu(other.on_gpu) {}
+// Tensor::Tensor(const Tensor &other) : host_data(other.host_data), device_data(nullptr),
+// 									  N(other.N), C(other.C), H(other.H), W(other.W), on_gpu(other.on_gpu) {}
 
-void Tensor::resize(int n, int c, int h, int w) {
-	if(n < 0 || c < 0 || h < 0 || w < 0)
+void Tensor::resize(int n, int c, int h, int w)
+{
+	if (n < 0 || c < 0 || h < 0 || w < 0)
 		throw std::invalid_argument("Tensor dimensions must be non-negative");
 
 	N = n;
@@ -41,27 +48,48 @@ size_t Tensor::size() const { return host_data.size(); }
 
 bool Tensor::is_gpu() const { return on_gpu; }
 
+// float &Tensor::operator()(int n, int c, int h, int w)
+// {
+// 	if (on_gpu)
+// 		return device_data[index(n, c, h, w)];
+// 	return host_data.at(index(n, c, h, w));
+// }
+
+// const float &Tensor::operator()(int n, int c, int h, int w) const
+// {
+// 	if (on_gpu)
+// 		return device_data[index(n, c, h, w)];
+// 	return host_data.at(index(n, c, h, w));
+// }
+
 float &Tensor::operator()(int n, int c, int h, int w) {
-	if(on_gpu) return device_data[index(n, c, h, w)];
-	return host_data.at(index(n, c, h, w));
+    if (on_gpu)
+        throw std::runtime_error("Accessing GPU tensor from CPU");
+    return host_data.at(index(n, c, h, w));
 }
 
 const float &Tensor::operator()(int n, int c, int h, int w) const {
-	if(on_gpu) return device_data[index(n, c, h, w)];
-	return host_data.at(index(n, c, h, w));
+    if (on_gpu)
+        throw std::runtime_error("Accessing GPU tensor from CPU");
+    return host_data.at(index(n, c, h, w));
 }
 
-float* Tensor::data() {
-	if(on_gpu) return device_data;
+float *Tensor::data()
+{
+	if (on_gpu)
+		return device_data;
 	return host_data.data();
 }
 
-const float* Tensor::data() const {
-	if(on_gpu) return device_data;
+const float *Tensor::data() const
+{
+	if (on_gpu)
+		return device_data;
 	return host_data.data();
 }
 
-std::size_t Tensor::index(int n, int c, int h, int w) const {
+std::size_t Tensor::index(int n, int c, int h, int w) const
+{
 	if (n < 0 || n >= N || c < 0 || c >= C || h < 0 || h >= H || w < 0 || w >= W)
 		throw std::out_of_range("Tensor indices out of range");
 
