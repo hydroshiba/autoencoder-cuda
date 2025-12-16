04f8ac2446ce15205a17eabc881f7cec2f047b06
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2025-12-16 10:01:08 +0700

    Fix: add kernel loss GPU and change num epoch, batches

--- source/tensor.cu
+++ source/tensor.cu
@@ -1,71 +1,72 @@
 #include "tensor.hpp"
 #include <kernel.cuh>
+#include <algorithm>
+#include <cuda_runtime.h>
 
 Tensor &Tensor::operator=(const Tensor &other)
 {
-	if (this == &other)
-		return *this;
+    if (this == &other)
+        return *this;
 
-	// We don't care about device data, only copy host data for consistency
+    host_data = other.host_data;
+    N = other.N;
+    C = other.C;
+    H = other.H;
+    W = other.W;
 
-	host_data = other.host_data;
-	N = other.N;
-	C = other.C;
-	H = other.H;
-	W = other.W;
+    if (device_data)
+    {
+        CHECK(cudaFree(device_data));
+        device_data = nullptr;
+    }
+    on_gpu = false;
 
-	if (device_data)
-	{
-		CHECK(cudaFree(device_data));
-		device_data = nullptr;
-		on_gpu = false;
-	}
-
-	return *this;
+    return *this;
 }
 
 void Tensor::clear_tensor()
 {
-	if (on_gpu)
-	{
-		cudaMemset(device_data, 0, host_data.size() * sizeof(float));
-	}
-	else
-	{
-		std::fill(host_data.begin(), host_data.end(), 0.0f);
-	}
+    if (on_gpu && device_data)
+    {
+        cudaMemset(device_data, 0, host_data.size() * sizeof(float));
+    }
+    else
+    {
+        std::fill(host_data.begin(), host_data.end(), 0.0f);
+    }
 }
 
 Tensor::~Tensor()
 {
-	if (device_data)
-	{
-		CHECK(cudaFree(device_data));
-		device_data = nullptr;
-	}
+    if (device_data)
+    {
+        CHECK(cudaFree(device_data));
+        device_data = nullptr;
+    }
 }
 
 Tensor &Tensor::to_gpu()
 {
-	if (on_gpu)
-		return *this;
-	on_gpu = true;
+    if (on_gpu)
+        return *this;
 
-	CHECK(cudaMalloc(&device_data, host_data.size() * sizeof(float)));
-	CHECK(cudaMemcpy(device_data, host_data.data(), host_data.size() * sizeof(float), cudaMemcpyHostToDevice));
-	return *this;
+    on_gpu = true;
+
+    CHECK(cudaMalloc(&device_data, host_data.size() * sizeof(float)));
+    CHECK(cudaMemcpy(device_data, host_data.data(), host_data.size() * sizeof(float), cudaMemcpyHostToDevice));
+
+    return *this;
 }
 
 Tensor &Tensor::to_cpu()
 {
-	if (!on_gpu)
-		return *this;
-	on_gpu = false;
+    if (!on_gpu || !device_data)
+        return *this;
 
-	cudaMemcpy(host_data.data(), device_data, host_data.size() * sizeof(float), cudaMemcpyDeviceToHost);
+    CHECK(cudaMemcpy(host_data.data(), device_data, host_data.size() * sizeof(float), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(device_data));
+    device_data = nullptr;
+    on_gpu = false;
 
-	CHECK(cudaFree(device_data));
-	device_data = nullptr;
-
-	return *this;
+    return *this;
 }
