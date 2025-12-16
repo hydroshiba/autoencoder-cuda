7e8d6717237102473e6c0453c879873a86a1fe26
Hieu Nguyen Huu Gia <nhghieu22@clc.fitus.edu.vn>
2025-12-16 15:14:09 +0700

    Add: Add inference code in encode.cpp
    - Load pre-trained Autoencoder
    - Run Encoder to encode images
    - Take the encoded Tensor to evaluate model's robustness

--- encode.cpp
+++ encode.cpp
@@ -1,5 +1,26 @@
-int main(int argc, char **argv)
+#include <iostream>
+#include "header/trainer.hpp"
+#include "header/dataset.hpp"
+#include "header/autoencoder.hpp"
+
+int main()
 {
-    // TODO: Logic to encode the dataset using trained autoencoder model goes here
+    std::cout << "[MAIN] Inference process started\n";
+
+    Autoencoder::Base *model = new Autoencoder::CPU();
+    std::cout << "[MAIN] Model created\n";
+
+    model->load_model("checkpoint.dat");
+    std::cout << "[MAIN] Checkpoint loaded\n";
+
+    DataLoader *data_loader = new DataLoader("cifar-10-batches-bin");
+    Tensor testImage = data_loader->get_batch(1,1);
+
+    Tensor inferedEncoding = model->encode(testImage);
+
+    // TODO: Add evaluation code here
+   
+    std::cout << "[MAIN] Evaluation finished\n";
+
     return 0;
 }
