6ffcc1ed741da6e772e4577ceb577320dc304573
hydroshiba <phanhaiminh.hcm@gmail.com>
2025-12-19 13:43:57 +0700

    Add: Training pipeline

--- train.cpp
+++ train.cpp
@@ -1,1 +1,28 @@
-// Logic to train the autoencoder model goes here
+#include "dataset.hpp"
+#include "optimizer.hpp"
+#include "loss.cuh"
+
+#include "autoencoder.hpp"
+#include "trainer.hpp"
+
+#include "utils/logger.hpp"
+
+int main() {
+    LOG("Starting Autoencoder Training Pipeline");
+
+    Config::load("config.yaml");
+
+    Autoencoder<Device::GPU> model;
+	model.build();
+
+	Dataset dataset("dataset/cifar-10-batches-bin");
+	Optimizer::SGD optimizer;
+
+	Trainer trainer;
+	trainer.fit(model, dataset, optimizer, Loss::MSE());
+    LOG("Training completed");
+
+    std::string checkpoint_path = "checkpoint/gpu_autoencoder.dat";
+    model.save(checkpoint_path);
+    LOG("Model saved to", checkpoint_path);
+}
